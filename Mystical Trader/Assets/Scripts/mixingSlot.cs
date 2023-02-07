373f862facb95cd26708eef671639c05034f53fb
HarrisonLeswick <72412866+HarrisonLeswick@users.noreply.github.com>
2023-02-07 02:32:29 -0500

    Pot spawn fully working
    spawns the empty pot prefab and makes it a child of the mixing slot

--- Mystical Trader/Assets/Scripts/mixingSlot.cs
+++ Mystical Trader/Assets/Scripts/mixingSlot.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class mixingSlot : MonoBehaviour, IDropHandler
 {
+    public GameObject emptyPotPrefab;
+    private GameObject emptyPot;
+
     public void OnDrop(PointerEventData eventData) {
 
-       GameObject dropped = eventData.pointerDrag;
-        Debug.Log("Mixing slot draggable landed");
+       GameObject dropped = eventData.pointerDrag;       
         if (dropped.tag == "Plant"){
             
             pickup pickup = dropped.GetComponent<pickup>();
@@ -16,7 +18,10 @@ public class mixingSlot : MonoBehaviour, IDropHandler
 
         
         if (dropped.tag =="SpawnNew"){
-               Debug.Log("spawn new time");
+               Instantiate(emptyPotPrefab, new Vector3(90.0f, -65, 0), Quaternion.identity);
+                emptyPot = GameObject.Find("/EmptyPot(Clone)");
+                emptyPot.transform.SetParent(gameObject.transform);
+
 
         }
 
--- Mystical Trader/Assets/Scripts/potss.cs
+++ Mystical Trader/Assets/Scripts/potss.cs
@@ -6,25 +6,25 @@ using UnityEngine.EventSystems;
 
 public class potss : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
- public GameObject emptyPotPrefab;
+ public GameObject potDraggable;
 public Vector3 basePosition; 
  
  private void Start() { 
-        basePosition = emptyPotPrefab.transform.position;
+        basePosition = potDraggable.transform.position;
     }
 
     public void OnBeginDrag(PointerEventData evemtData){
-       emptyPotPrefab.transform.SetParent(transform.root); // becomes a child off the UI 
-       emptyPotPrefab.transform.SetAsLastSibling(); //becomes the last child this makes it so it is the closest to the camera and wont pass under things
+       potDraggable.transform.SetParent(transform.root); // becomes a child off the UI 
+       potDraggable.transform.SetAsLastSibling(); //becomes the last child this makes it so it is the closest to the camera and wont pass under things
     }
 
   public void OnDrag(PointerEventData evemtData){
-        emptyPotPrefab.transform.position = Input.mousePosition;
+        potDraggable.transform.position = Input.mousePosition;
        
   }
 
   public void OnEndDrag(PointerEventData evemtData){
-     emptyPotPrefab.transform.position = basePosition;
+     potDraggable.transform.position = basePosition;
 
   }
 }
